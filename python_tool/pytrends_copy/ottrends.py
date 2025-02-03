53ea61db98a07b8605430d732e5bc143110c8643
KeisukeOta <k.ota109@outlook.com>
2025-02-04 02:06:21 +0900

    trends実装開始

---
+++ python_tool/pytrends_copy/ottrends.py
@@ -0,0 +1,25 @@
+import json
+
+import pandas as pd
+import requests
+
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests import status_codes
+
+from pytrends import exceptions
+
+from urllib.parse import quote
+
+BASE_TRENDS_URL = "https://trends.google.com/trends"
+
+class TrendReq(object):
+    
+    def __init__():
+        pass
+
+    def build_payload():
+        pass
+
+    def interest_over_time():
+        pass
