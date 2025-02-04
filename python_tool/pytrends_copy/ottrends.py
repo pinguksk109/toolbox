4f1dd318056ed237e7a09007470a33720bc58cd5
Keisuke Ota <41576749+pinguksk109@users.noreply.github.com>
2025-02-04 23:56:50 +0900

    Update ottrends.py

--- python_tool/pytrends_copy/ottrends.py
+++ python_tool/pytrends_copy/ottrends.py
@@ -14,12 +14,166 @@ from urllib.parse import quote
 BASE_TRENDS_URL = "https://trends.google.com/trends"
 
 class TrendReq(object):
-    
-    def __init__():
-        pass
+    GET_METHOD = 'get'
+    POST_METHOD = 'post'
+    GENERAL_URL = f'{BASE_TRENDS_URL}/api/explore'
+    INTEREST_OVER_TIME_URL = f'{BASE_TRENDS_URL}/api/widgetdata/multiline'
+    MULTIRANGE_INTEREST_OVER_TIME_URL = f'{BASE_TRENDS_URL}/api/widgetdata/multirange'
+    INTEREST_BY_REGION_URL = f'{BASE_TRENDS_URL}/api/widgetdata/comparedgeo'
+    RELATED_QUERIES_URL = f'{BASE_TRENDS_URL}/api/widgetdata/relatedsearches'
+    TRENDING_SEARCHES_URL = f'{BASE_TRENDS_URL}/hottrends/visualize/internal/data'
+    TOP_CHARTS_URL = f'{BASE_TRENDS_URL}/api/topcharts'
+    SUGGESTIONS_URL = f'{BASE_TRENDS_URL}/api/autocomplete/'
+    CATEGORIES_URL = f'{BASE_TRENDS_URL}/api/explore/pickers/category'
+    TODAY_SEARCHES_URL = f'{BASE_TRENDS_URL}/api/dailytrends'
+    REALTIME_TRENDING_SEARCHES_URL = f'{BASE_TRENDS_URL}/api/realtimetrends'
+    ERROR_CODES = (500, 502, 504, 429)
 
-    def build_payload():
-        pass
+    def __init__(self, hl='en-US', tz=360, geo='', timeout=(2,5), proxies='', retries=0, backoff_factor=0, requests_args=None):
+        self.google_rl = 'You have reached your quota limit. Please try again later.'
+        self.results = None
+        self.tz = tz
+        self.hl = hl
+        self.geo = geo
+        self.kw_list = list()
+        self.timeout = timeout
+        self.proxies = proxies
+        self.retries = retries
+        self.backoff_factor = backoff_factor
+        self.proxy_index = 0
+        self.requests_args = requests_args or {}
+        self.cookies = self.GetGoogleCookie()
+        self.token_payload = dict()
+        self.interest_over_time_widget = dict()
+        self.interest_by_region_widget = dict()
+        self.related_topics_widget_list = list()
+        self.related_queries_widget_list = list()
 
-    def interest_over_time():
-        pass
+        self.headers = {'accept-language': self.hl}
+        self.headers.update(self.requests_args.pop('headers', {}))
+
+    def GetGoogleCookie(self):
+        while True:
+            print("proxies" in self.requests_args)
+            print(self.requests_args)
+            if "proxies" in self.requests_args:
+                try:
+                    return dict(filter(lambda i: i[0] == 'NID', requests.get(
+                        f'{BASE_TRENDS_URL}/explore/?geo={self.hl[-2:]}',
+                        timeout=self.timeout,
+                        **self.requests_args
+                    ).cookies.items()))
+                except:
+                    continue
+            else:
+                print(len(self.proxies) > 0)
+                if len(self.proxies) > 0:
+                    proxy = {'https': self.proxies[self.proxy_index]}
+                else:
+                    proxy = ''
+                try:
+                    print(filter(lambda i: i[0] == 'NID', requests.get(
+                        f'{BASE_TRENDS_URL}/explore/?geo={self.hl[-2:]}',
+                        timeout=self.timeout,
+                        proxies=proxy,
+                        **self.requests_args).cookies))
+                    return dict(filter(lambda i: i[0] == 'NID', requests.get(
+                        f'{BASE_TRENDS_URL}/explore/?geo={self.hl[-2:]}',
+                        timeout=self.timeout,
+                        proxies=proxy,
+                        **self.requests_args
+                    ).cookies.items()))
+                except requests.exceptions.ProxyError:
+                    print('Proxy error. Changing IP')
+                    if len(self.proxies) > 1:
+                        self.proxies.remove(self.proxies[self.proxy_index])
+                    else:
+                        print('No more proxies available. Bye!')
+                        raise
+                    continue
+
+    def build_payload(self, kw_list, cat=0, timeframe='today 5-y', geo='', gprop=''):
+        if gprop not in ['', 'images', 'news', 'youtube', 'froogle']:
+            raise ValueError('gprop must be empty (to indicate web), images, news, youtube, or froogle')
+        self.kw_list = kw_list
+        self.geo = geo or self.geo
+        self.token_payload = {
+            'hl': self.hl,
+            'tz': self.tz,
+            'req': {'comparisonItem': [], 'category': cat, 'propery': gprop}
+        }
+
+        if isinstance(timeframe, list):
+            for index, kw in enumerate(self.kw_list):
+                keyword_payload = {'keyword': kw, 'time': timeframe[index], 'geo': self.geo}
+                self.token_payload['req']['comparisonItem'].append(keyword_payload)
+        else:
+            for kw in self.kw_list:
+                keyword_payload = {'keyword': kw, 'time': timeframe, 'geo': self.geo}
+                self.token_payload['req']['comparisonItem'].append(keyword_payload)
+
+        self.token_payload['req'] = json.dumps(self.token_payload['req'])
+        self._token()
+        return
+
+    def _token(self):
+        widget_dicts = self._get_data(
+            url=TrendReq.GENERAL_URL,
+            method=TrendReq.POST_METHOD,
+            params=self.token_payload,
+            trim_chats=4
+        )['widgets']
+        first_region_token = True
+        self.related_queries_widget_list[:] = []
+        self.related_topics_widget_list[:] = []
+        for widget in widget_dicts:
+            if widget['id'] == 'TIMESERIES':
+                self.interest_over_time_widget = widget
+            if widget['id'] == 'GEO_MAP' and first_region_token:
+                self.interest_by_region_widget = widget
+                first_region_token = False
+            if 'RELATED_TOPICS' in widget['id']:
+                self.related_topics_widget_list.append(widget)
+            if 'RELATED_QUERIES' in widget['id']:
+                self.related_queries_widget_list.append(widget)
+        return
+
+    def _get_data(self, url, method=GET_METHOD, trim_chars=0, **kwargs):
+        s = requests.session()
+        if self.retries > 0 or self.backoff_factor > 0:
+            retry = Retry(total=self.retries, read=self.retries,
+                          connect=self.retries,
+                          backoff_factor=self.backoff_factor,
+                          status_forcelist=TrendReq.ERROR_CODES,
+                          method_whitelist=frozenset(['GET', 'POST']))
+        s.mount('http://', HTTPAdapter(max_retries=retry))
+
+        s.headers.update(self.headers)
+        if len(self.proxies) > 0:
+            self.cookies = self.GetGoogleCookie()
+            s.proxies.update({'https': self.proxies[self.proxy_index]})
+        if method == TrendReq.POST_METHOD:
+            response = s.post(url, timeout=self.timeout,
+                              cookies=self.cookies, **kwargs,
+                              **self.requests_args)
+        else:
+            response = s.get(url, timeout=self.timeout, cookies=self.cookies,
+                             **kwargs, **self.requests_args)
+        if response.status_code == 200 and 'application/json' in \
+                response.headers['Content-Type'] or \
+                'application/javascript' in response.headers['Content-Type'] or \
+                'text/javascript' in response.headers['Content-Type']:
+            content = response.text[trim_chars:]
+            self.GetNewProxy()
+            return json.loads(content)
+        else:
+            if response.status_code == status_codes.codes.too_many_requests:
+                raise exceptions.TooManyRequestsError.from_response(response)
+            raise exceptions.ResponseError.from_response(response)
+
+
+    def GetNewProxy(self):
+        if self.proxy_index < (len(self.proxies) - 1):
+            self.proxy_index += 1
+        else:
+            self.proxy_index = 0
